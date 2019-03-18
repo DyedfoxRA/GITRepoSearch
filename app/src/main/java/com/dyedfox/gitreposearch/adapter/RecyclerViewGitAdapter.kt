35fe27c78e7295d16f44028191ce3afd9ba8e035
dyedfox <dyedfox.ra@gmail.com>
2019-03-18 22:48:26 +0100

    dialog

--- app/src/main/java/com/dyedfox/gitreposearch/adapter/RecyclerViewGitAdapter.kt
+++ app/src/main/java/com/dyedfox/gitreposearch/adapter/RecyclerViewGitAdapter.kt
@@ -1,18 +1,39 @@
 package com.dyedfox.gitreposearch.adapter
 
+import android.app.Dialog
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.dyedfox.gitreposearch.R
 import com.dyedfox.gitreposearch.model.entity.Item
 
 class RecyclerViewGitAdapter(val listOfItems: List<Item>, val context: Context) :
     RecyclerView.Adapter<RecyclerViewGitViewHolder>() {
+
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewGitViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        return RecyclerViewGitViewHolder(layoutInflater.inflate(R.layout.repo_item, parent, false))
+
+        val vh = RecyclerViewGitViewHolder(layoutInflater.inflate(R.layout.repo_item, parent, false))
+
+        vh.itemView.setOnClickListener {
+
+            val dg = Dialog(context)
+
+            dg.setContentView(R.layout.repo_dialog)
+
+            dg.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+            dg.show()
+        }
+
+        return vh
     }
 
     override fun getItemCount(): Int {
@@ -37,6 +58,8 @@ class RecyclerViewGitAdapter(val listOfItems: List<Item>, val context: Context) 
         holder.getDescription().let {
             it.text = item.description
         }
+
+
     }
 }
 
