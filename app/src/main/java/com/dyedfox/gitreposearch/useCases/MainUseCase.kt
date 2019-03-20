a76875963384eb4184c36a783cc0f30f1b07258c
dyedfox <dyedfox.ra@gmail.com>
2019-03-20 09:48:37 +0100

    dialog/ network check

--- app/src/main/java/com/dyedfox/gitreposearch/useCases/MainUseCase.kt
+++ app/src/main/java/com/dyedfox/gitreposearch/useCases/MainUseCase.kt
@@ -8,14 +8,15 @@ import com.dyedfox.gitreposearch.model.entity.Item
 class MainUseCase(context_: Context) : BaseUseCase(context_) {
 
     suspend fun getReposResponse(name : String): List<Item>? {
-
-        val resp = getNetworkHelper()
+        if(checkNetworkConnection())
+        {val resp = getNetworkHelper()
             .client
             .getRepos(name)
             .await()
             .body()?.items
 
         Log.e("res",resp.toString())
-            return resp
+            return resp}
+        else return emptyList()
     }
 }
